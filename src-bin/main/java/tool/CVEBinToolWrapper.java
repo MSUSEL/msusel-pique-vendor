7cd6ffd314d0a6d2b83cbc2d38044f5ae2909a7f
Ernesto Ortiz <ernesto.ortiz@upr.edu>
2021-06-20 16:47:57 -0400

    Changed second argument of constructor to the path of the flawfinder executable file. Also change value of cmd string in initialize method to cmd to run flawfinder on a file.

--- src-bin/main/java/tool/CVEBinToolWrapper.java
+++ src-bin/main/java/tool/CVEBinToolWrapper.java
@@ -51,7 +51,7 @@ public class CVEBinToolWrapper extends Tool implements ITool  {
 	
 			
 	public CVEBinToolWrapper() {
-		super("cve-bin-tool", null);
+		super("cve-bin-tool", "C:/Users/ernes/AppData/Local/Programs/Python/Python38-32/Scripts/flawfinder.exe");
 		
 	}
 
@@ -144,7 +144,7 @@ public class CVEBinToolWrapper extends Tool implements ITool  {
 			//NOTE: the version of cve-bin-tool that is installed at the time of writing this will error when downloading CVEs
 			//However, this will be the command that should be run in the future. If this is failing, get the working
 			//version and make this cmd something unimportant. 
-			final String cmd = "cmd /c python -m pip install cve-bin-tool"; 
+			final String cmd = "flawfinder --csv flawFinderFile.c > flawResults.csv";
 			
 			Process p;
 			try {
