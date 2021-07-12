d1278579ca2d3ab9a567dbdfb25063b06bab8ae0
Ernesto Ortiz <ernesto.ortiz@upr.edu>
2021-07-12 19:50:26 -0400

    Refactored some names and removed unused files.

--- src/main/java/utilities/PiqueProperties.java
+++ src/main/java/utilities/PiqueProperties.java
@@ -31,7 +31,7 @@ public class PiqueProperties {
 
         Properties prop = new Properties();
         try {
-            prop.load(new FileReader("C:\\Users\\ernes\\IdeaProjects\\myVendor2\\src\\main\\resources\\pique-bin.properties"));
+            prop.load(new FileReader("C:\\Users\\ernes\\IdeaProjects\\myVendor2\\src\\main\\resources\\piqueVendor.properties"));
 
         }catch(Exception e){
             e.printStackTrace();
